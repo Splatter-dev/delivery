78122b3d44f02e68279091aa869bddd2c32d0107
Paulo <paulovsp16@hotmail.com>
2021-06-23 16:38:10 -0300

    Make app instalable

---
+++ Makefile
@@ -0,0 +1,4 @@
+install:
+	pip install -e .['dev'] 
+
+# ['dev'] instala os requirements-dev.txt também. extras_require do setup.py
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+def read(file_name):
+    return [item.strip() for item in open(file_name).readlines()]
+
+
+setup(
+    name="delivery",
+    version="0.1.0",  # major #minor #patch
+    description="Delivery app",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=read('requirements.txt'),
+    extras_require={
+        "dev": read('requirements-dev.txt')
+    }
+
+)
